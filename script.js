e3107f11764ef7878035c49b1bc1cafacf8d6589
Faradayplus <129697154+Faradayplus@users.noreply.github.com>
2023-04-07 22:30:26 -0400

    update

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ Seccion: 411
 */
 
 /* GENERACIONES Y CALCULOS DE LANZAMIENTO DE DARDOS */
-const total_iteraciones = 165; // Cantidad de veces que se lanzan los dardos
-let cantidad_tiros = 600; // Cantidad de dardos lanzados, se aumenta n iteraciones su valor
-const aproximacion_PI = [];
+const total_iteraciones = 26; // Cantidad de veces que se lanzan los dardos, a mayor cantidad de veces mas tarda en ejecutar
+let cantidad_tiros = 32; // Cantidad de dardos lanzados, se aumenta n iteraciones su valor
+let cantidad_aux = cantidad_tiros; // Declarar un auxiliar sirve para que se realicen cambios en el grafico de manera automatica
+const epsilon = 0.0025; 
+const aproximacion_PI = []; 
 let dardosenDiana = 0;
 let dardosenCuadrado = 0;
 let labels = [];
@@ -27,14 +29,14 @@ const radio = tamaño_cuadrado / 2;
 const $cuerpoTabla = document.querySelector("#cuerpoTabla");
 
 
-for (let i = 0; i < total_iteraciones; i++) {
+for (let i = 0; i < total_iteraciones; i++){
   //En cada iteración del bucle for, el valor de i se incrementa en 1 y se agrega al final del vector labels. Al final de todas las iteraciones, el vector labels contendrá una secuencia de números enteros que va desde 0 hasta total_iteraciones - 1
   labels.push(i);
 
-  dardosenDiana = 0;
+  dardosenDiana = 0; // Estas variables estan dentro del for para que no se incrementen cada vez que se recorra el bucle
   dardosenCuadrado = 0;
 
-  for (let j = 0; j < cantidad_tiros; j++) {
+  for (let j = 0; j < cantidad_tiros; j++){
     // La función Math.random() genera un número aleatorio entre 0 y 1, y luego se multiplica por tamaño_cuadrado para obtener un valor aleatorio dentro del rango del tamaño del cuadrado
     const x = Math.random() * tamaño_cuadrado;
     const y = Math.random() * tamaño_cuadrado;
@@ -44,28 +46,32 @@ for (let i = 0; i < total_iteraciones; i++) {
       Math.pow(x - radio, 2) + Math.pow(y - radio, 2)
     );
     // Si la distancia del dardo calculada anteriormente es menor o igual al radio eso significa que esta dentro de la diana y si la distancia es mayor eso implica que cae en el cuadrado
-    if (distancia <= radio) {
+    // El uso de epsilon es una forma de permitir un margen de error en la aproximación del valor de pi, ya que algunos dardos pueden estar muy cerca del límite del círculo pero aún así ser considerados fuera del mismo debido a la precisión de las operaciones 
+    if (distancia <= radio + epsilon){
       dardosenDiana++;
     } else {
       dardosenCuadrado++;
     }
-
-    if (i == total_iteraciones - 1) {
-      // Creamos un punto y le asignno la clase adecuada dependiendo si está dentro o fuera del círculo
-      const dardos = document.createElement("div");
-      dardos.setAttribute("id", "dardos");
-
-      if (distancia <= radio) {
-        dardos.classList.add("dentro_circulo");
-      } else {
-        dardos.classList.add("fuera_circulo");
-      }
-//Se agregan los dardos generados en la simulación a la pantalla y "px" es para indicar que se está utilizando la unidad de medida en píxeles. De esta manera, se le está diciendo al compilador que coloque el dardo en una posición específica de la pantalla en función de su posición en "x" o en "y".
-      dardos.style.top = y + "px";
-      dardos.style.left = x + "px";
-      cuadrado.appendChild(dardos);
-    }
   }
+
+  //En el caso de que se quiera mostrar el cuadrado impreso se debe habilitar el codigo comentado
+  //
+  /*if (i == total_iteraciones - 1){
+    // Creamos un punto y le asignno la clase adecuada dependiendo si está dentro o fuera del círculo
+    const dardos = document.createElement("div");
+    dardos.setAttribute("id", "dardos");
+
+    if (distancia <= radio){
+      dardos.classList.add("dentro_circulo");
+    } 
+    else{
+      dardos.classList.add("fuera_circulo");
+    }
+    //Se agregan los dardos generados en la simulación a la pantalla y "px" es para indicar que se está utilizando la unidad de medida en píxeles. De esta manera, se le está diciendo al compilador que coloque el dardo en una posición específica de la pantalla en función de su posición en "x" o en "y".
+    //dardos.style.top = y + "px";
+    //dardos.style.left = x + "px";
+    //cuadrado.appendChild(dardos);
+  }*/
 
   // Formula para calcular la aproximacion de PI
   const formula_PI = 4 * (dardosenDiana / cantidad_tiros);
@@ -78,7 +84,7 @@ for (let i = 0; i < total_iteraciones; i++) {
 
   //Creo una nueva celda de datos para la columna de iteraciones y se va mostrando para cada iteracion i+1 o las variables correspondientes.
   // let $td(DatosDeTabla) = document.createElement("td"): crea una nueva celda de datos para la columna correspondiente.
-   //appendChild se utiliza en este código para agregar una nueva fila de datos a una tabla HTML
+  //appendChild se utiliza en este código para agregar una nueva fila de datos a una tabla HTML
   let $tdIteracion = document.createElement("td");
   $tdIteracion.textContent = i+1;
   $tr.appendChild($tdIteracion);
@@ -95,15 +101,12 @@ for (let i = 0; i < total_iteraciones; i++) {
 
   
   let $tdPi = document.createElement("td");
-  $tdPi.textContent = formula_PI//.toFixed(2);
+  $tdPi.textContent = formula_PI.toFixed(8);
   $tr.appendChild($tdPi);
 
   $cuerpoTabla.appendChild($tr);
-
-  cantidad_tiros += 600;
- 
+  cantidad_tiros *= 2; // Aumentando 2 veces el valor por cada iteracion se aplica la Martin Gala
 }
-
 /* GRAFICO */
 
 // Obtengo la referencia al HTML al elemento, LO usO para configurar y mostrar el grafico
@@ -113,7 +116,7 @@ const ctx = canvas.getContext("2d");
 new Chart(ctx, {
   type: "line",
   data: {
-    labels: Array.from({ length: aproximacion_PI.length }, (_, i) => (i + 1) * 600),
+    labels: Array.from({ length: aproximacion_PI.length }, (_, i) => (i + 1) * cantidad_aux),
     datasets: [
       {
         label: "Aproximación de PI",
@@ -126,7 +129,7 @@ new Chart(ctx, {
   },
   options: {
     responsive: true,
-    plugins: {
+    plugins:  {
       legend: {
         display: false,
       },
@@ -135,7 +138,7 @@ new Chart(ctx, {
       x: {
         title: {
           display: true,
-          text: "Cantidad de simulaciones",
+          text: "Número de dardos",
         },
       },
       y: {
@@ -143,8 +146,8 @@ new Chart(ctx, {
           display: true,
           text: "Aproximación de PI (π)",
         },
-        suggestedMin: 3.10,
-        suggestedMax: 3.18,
+        suggestedMin: 3.00,
+        suggestedMax: 3.20,
         ticks: {
           precision: 4,
         },
